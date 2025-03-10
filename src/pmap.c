9c28a3f9866b3f25778fa251c6fb1b238a9aa0b5
r32 <56782055@qq.com>
2025-03-10 22:47:36 +0800

    pmap license

--- src/pmap.c
+++ src/pmap.c
@@ -1,5 +1,25 @@
 /*
  * PMap in C language, This code is ported from OCaml ExtLib PMap
+ * Copyright (C) 2025 LiuWM
+ */
+/*
+ * PMap - Polymorphic maps
+ * Copyright (C) 1996-2003 Xavier Leroy, Nicolas Cannasse, Markus Mottl
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version,
+ * with the special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include "pmap.h"
