5e991c694c8f8103fc262d979ed15b284f2fa486
r32 <56782055@qq.com>
2025-05-13 15:30:50 +0800

    rename legacy ucs2.c to utf8.c

--- include/utf8.h
+++ include/utf8.h
@@ -1,12 +1,12 @@
 /*
+ * Legacy
+ * 
  * SPDX-License-Identifier: GPL-2.0
  */
 
-#ifndef R_UCS2_H
-#define R_UCS2_H
-
-#include "rclibs.h"
-C_FUNCTION_BEGIN
+#ifndef LWM_UTF8_H
+#define LWM_UTF8_H
+#include <stddef.h>
 
 /*
  * @param srclen/srcbytes : If this parameter is -1, the function processes the entire input string,
@@ -17,10 +17,7 @@ C_FUNCTION_BEGIN
  * number of characters. If the provided size does not include a terminating null character,
  * the resulting string is not null-terminated, and the returned length does not include this character.
  */
-
 int wcstoutf8(unsigned char *out, const unsigned short *src, int srclen);
-
 int utf8towcs(unsigned short *out, const unsigned char *src, int srcbytes);
 
-C_FUNCTION_END
 #endif
